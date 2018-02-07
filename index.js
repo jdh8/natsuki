984e1952812624832b77e622aac263ddd7940799
Chen-Pang He <chen.pang.he@jdh8.org>
2018-02-07 15:49:31 +0800

    OO command handler

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 "use strict";
 
-const Discord = require("discord.js");
-const natsuki = new Discord.Client();
-
-natsuki.on("message", message =>
+const natsuki =
 {
-	if (message.content == "n.ping")
-		message.reply("pong!");
+	ping(message) { message.reply("pong!"); }
+};
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("message", message =>
+{
+	const match = /^n\.(\S*)\s*(.*)/.exec(message.content);
+
+	if (match) {
+		const callback = natsuki[match[1]];
+
+		if (callback)
+			callback(message, match[2]);
+	}
 });
 
-natsuki.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
