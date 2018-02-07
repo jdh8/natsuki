e499dbdf4de2275f53c3c5f5bd791334a8816181
Chen-Pang He <chen.pang.he@jdh8.org>
2018-02-07 14:49:42 +0800

    Simplify everything with discord.js

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 "use strict";
 
-function Natsuki()
+const Discord = require("discord.js");
+const natsuki = new Discord.Client();
+
+natsuki.on("message", message =>
 {
-	const Discord = require("discord.io");
-	const conf = require("./auth.json");
-
-	conf.autorun = true;
-
-	return new Discord.Client(conf);
-}
-
-const natsuki = Natsuki();
-
-natsuki.on("message", function(username, user, channel, message)
-{
-	const mention = "<@" + user + ">";
-	
-	if (message == "n.ping") {
-		this.sendMessage({
-			to: channel,
-			message: mention + ": pong!"
-		});
-	}
+	if (message.content == "n.ping")
+		message.reply("pong!");
 });
+
+natsuki.login(process.env.TOKEN);
