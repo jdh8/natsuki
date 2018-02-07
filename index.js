664a6caa8a35063a77a948a4dc78a7b2a90c5b29
Chen-Pang He <chen.pang.he@jdh8.org>
2018-02-08 02:37:59 +0800

    Improve help message of a non-existent command

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const natsuki =
 	ping() { return "Pong!"; },
 	repo() { return "https://github.com/yurigang/natsuki"; },
 	eval(message) { return eval(message.content); },
-	help(message) { return manual[/\S*/.exec(message.content)] || manual[""]; },
+
+	help(message)
+	{
+		const command = /\S*/.exec(message.content);
+		return manual[command] || `The command \`${command}\` is not found.`;
+	},
 };
 
 const Discord = require("discord.js");
