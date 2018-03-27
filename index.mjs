9eb63d323e8924b8c783002c1d192529fef4666f
Chen-Pang He <chen.pang.he@jdh8.org>
2018-03-27 16:06:03 +0800

    Stop looping of n.poll

--- index.mjs
+++ index.mjs
@@ -428,7 +428,7 @@ client.on("ready", () =>
 
 client.on("message", message =>
 {
-	const match = /^n\.(\S*)\s*([^]*)/.exec(message.content);
+	const match = !message.author.bot && /^n\.(\S*)\s*([^]*)/.exec(message.content);
 	const f = match && natsuki.hasOwnProperty(match[1]) && natsuki[match[1]];
 	const promise = f && f(message, match[2]);
 
