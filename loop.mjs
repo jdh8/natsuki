9bf3ee6cd11dbea36869f5e92c0949627eba53d4
Chen-Pang He <chen.pang.he@jdh8.org>
2018-04-06 02:03:10 +0800

    Completely disable DM

--- loop.mjs
+++ loop.mjs
@@ -1,8 +1,9 @@
+import Discord from "discord.js";
 import * as natsuki from "./index.mjs";
 
 export default message =>
 {
-	if (message.author.bot)
+	if (message.author.bot || message.channel instanceof Discord.DMChannel)
 		return;
 
 	const match = /^(?:n\.|(<@!?410315411695992833>)\s*)(\S*)\s*([^]*)/.exec(message.content);
