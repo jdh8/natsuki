aa5b4163d95eff7111738b0f896b66c3cadba10c
Chen-Pang He <chen.pang.he@jdh8.org>
2018-04-05 22:14:55 +0800

    Remove all easter eggs

--- loop.mjs
+++ loop.mjs
@@ -5,15 +5,11 @@ export default message =>
 	if (message.author.bot)
 		return;
 
-	const greet = message => message.react("408016032410894346");
 	const match = /^(?:n\.|(<@!?410315411695992833>)\s*)(\S*)\s*([^]*)/.exec(message.content);
 
 	if (match) {
 		const [, mention, command, content] = match;
 		const f = natsuki[command];
-		return f ? f(message, content) : mention && greet(message);
+		return f ? f(message, content, mention) : mention && message.react("408016032410894346");
 	}
-
-	if (/<@!?410315411695992833>/.test(message.content))
-		return greet(message);
 };
