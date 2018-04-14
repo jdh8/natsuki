1425575cafadbee817e048b9aed407ab7fd4a8d5
Chen-Pang He <chen.pang.he@jdh8.org>
2018-04-14 23:05:12 +0800

    Adjust n.rate algorithm

--- index.mjs
+++ index.mjs
@@ -232,9 +232,9 @@ export const poem = (message, content) =>
 
 export const rate = (message, content) =>
 {
-	const data = content || `${message.author}`;
-	const hash = new Uint32Array(crypto.createHash("md5").update(data.toLowerCase()).digest().buffer);
-	const percentage = ((hash[0] + 24) >>> 0) % 101;
+	const data = content ? content.toLowerCase().replace(/<@!(\d+)>/g, "<@$1>") : `${message.author}`;
+	const hash = new Uint32Array(crypto.createHash("md5").update(data).digest().buffer);
+	const percentage = ((hash[0] + 25) >>> 0) % 101;
 
 	return message.channel.send(`<:natsuki:424991419329937428> I'd give ${data} ${percentage}%.`);
 };
