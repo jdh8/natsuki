0f93b509d4630cba674d1f57ef766588aed27b78
Chen-Pang He <chen.pang.he@jdh8.org>
2018-04-15 03:51:37 +0800

    Add helpful error message to n.keycaps

--- index.mjs
+++ index.mjs
@@ -300,12 +300,20 @@ export const emojis = message =>
 	message.channel.send(`The list of emojis is on <#420885744077504532> on Natsuki's shelf, my support server.  Please check them out there.
 https://discord.gg/VdHYvMC`);
 
-export const keycaps = (message, content) => message.channel.send(content ?
-	content.toUpperCase()
+export const keycaps = (message, content) =>
+{
+	if (!content)
+		return message.channel.send("_ _");
+
+	const capped = content.toUpperCase()
 		.replace(/ /g, "\u2002")
 		.replace(/[0-9*#]/g, "$&\u20E3")
-		.replace(/[A-Z]/g, match => `${String.fromCodePoint(match.charCodeAt() + 0x1F1A5)}\xAD`) :
-	"_ _");
+		.replace(/[A-Z]/g, match => `${String.fromCodePoint(match.charCodeAt() + 0x1F1A5)}\xAD`);
+
+	const error = "Your message is too long!  Discord allows at most 2000 characters in a message and keycapping roughly doubles the length.  Therefore, try to cut you message down to 1000 characters.";
+
+	return message.channel.send(capped.length <= 2000 ? capped : error);
+};
 
 export const poll = (message, content) =>
 {
