db35d5587e78d78f285fb838d8a69e3dc99e8b76
Chen-Pang He <chen.pang.he@jdh8.org>
2018-05-04 01:28:45 +0800

    Implement n.roles

--- index.mjs
+++ index.mjs
@@ -435,6 +435,38 @@ export const avatar = (message, content) =>
 	return message.channel.send(url);
 };
 
+const best = (collection, name) =>
+{
+	const pattern = new RegExp(name, "i");
+	const filtered = collection.filterArray(x => pattern.test(x.name));
+
+	return filtered.length ? filtered.reduce((x, y) => x.name.length < y.name.length ? x : y) : null;
+};
+
+export const role = (message, content) =>
+{
+	if (!content)
+		return message.channel.send("Please specify role to search.");
+
+	const collection = message.guild.roles;
+	const mention = /<@&(\d+)>/.exec(content);
+	const role = mention ? collection.get(mention[1]) : best(collection, content);
+
+	if (role == null)
+		return message.channel.send("This role is not found.");
+
+	return message.channel.send(new Discord.RichEmbed({
+		color: role.color,
+		description: `${role} (${role.id})`,
+		fields: [
+			{ name: "Color", value: role.hexColor },
+			{ name: "Hoist", value: role.hoist },
+			{ name: "Managed", value: role.managed },
+			{ name: "Mentionable", value: role.mentionable },
+		]
+	}));
+};
+
 /******* NSFW *******/
 export const fuck = async (message, content) =>
 {
