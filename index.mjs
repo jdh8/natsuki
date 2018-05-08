f1985d714d9577f37f8fb76a12eff688eccb978f
Chen-Pang He <chen.pang.he@jdh8.org>
2018-05-08 20:12:34 +0800

    Implement n.color

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import Discord from "discord.js";
 import Jimp from "jimp";
 import snekfetch from "snekfetch";
+import tinycolor from "tinycolor2";
 import xml2js from "xml2js";
 
 import crypto from "crypto";
@@ -310,6 +311,25 @@ export const base64 = (message, content) =>
 		default:
 			return message.channel.send(manual.base64);
 	}
+};
+
+export const color = async (message, content) =>
+{
+	const color = new tinycolor(content);
+
+	if (!color.isValid())
+		return await message.channel.send(`${content} is not a color.`);
+
+	const description = `**Hex:** ${color.getAlpha() == 1 ? color.toHexString() : color.toHex8String()}
+**RGB:** ${color.toRgbString()}
+**HSL:** ${color.toHslString()}`;
+
+	const { r, g, b, a } = color.toRgb();
+	const rgba = Math.round(r) << 24 | Math.round(g) << 16 | Math.round(b) << 8 | Math.round(255 * a); 
+	const image = new Jimp(64, 64, rgba >>> 0);
+	const buffer = await util.promisify((...x) => image.getBuffer(...x))("image/png");
+
+	return await message.channel.send(description, new Discord.Attachment(buffer, "color.png"));
 };
 
 export const echo = (message, content) =>
