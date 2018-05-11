0949f052efc6db67c21cd9a50456ca9e3a31be90
Chen-Pang He <chen.pang.he@jdh8.org>
2018-05-12 02:10:51 +0800

    Fix mention-based things

--- util/User.mjs
+++ util/User.mjs
@@ -8,3 +8,9 @@ export const avatar = (user, size) =>
 
 	return `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}${extension}${query}`;
 };
+
+export const snowflake = string =>
+{
+	const match = /\d+/.exec(string);
+	return match ? match[0] : "";
+};
