17941f3fe3db07316824c830c02b2c01edf8e799
Chen-Pang He <chen.pang.he@jdh8.org>
2018-06-17 01:07:11 +0800

    Implement n.smash

--- modules/weeb.mjs
+++ modules/weeb.mjs
@@ -40,3 +40,8 @@ export const neko = Message.typing(async (message, content) =>
 		image: (await snekfetch.get(endpoint)).body,
 	}));
 });
+
+export const smash = (message, content) => message.channel.send(new Discord.RichEmbed({
+	description: `${message.author} smashed${content && " "}${content}!`,
+	image: { url: "https://raw.githubusercontent.com/jdh8/natsuki/master/assets/smash.png" },
+}));
