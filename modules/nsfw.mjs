a2f5857b78edbb1865acde3aeeed008165c7962a
Chen-Pang He <chen.pang.he@jdh8.org>
2018-07-10 03:08:07 +0800

    Fix position of n.fuck

--- modules/nsfw.mjs
+++ modules/nsfw.mjs
@@ -18,7 +18,7 @@ export const fuck = nsfw(Message.typing(async (message, content) =>
 	const user = message.client.users.get(/\d+|$/.exec(content)[0]);
 	const text = `${message.author} fucked ${user || "Natsuki"}`;
 	const image = Jimp.read("assets/566424ede431200e3985ca6f21287cee.png");
-	const composed = (await image).composite(await avatar(message.author), 364, 125);
+	const composed = (await image).composite(await avatar(message.author), 364, 100);
 
 	if (user)
 		composed.composite(await avatar(user), 110, 20);
