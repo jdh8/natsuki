2ff04a966f134bb006031679f6718ce1395b8812
Chen-Pang He <chen.pang.he@jdh8.org>
2018-12-13 13:18:55 +0800

    More readable modularization

--- data/index.mjs
+++ data/index.mjs
@@ -1,7 +1,9 @@
-export { default as hugs } from "./hugs.json";
-export { default as kisses } from "./kisses.json";
-export { default as manual } from "./manual.json";
-export { default as poetry } from "./poetry.json";
+import hugs from "./hugs.json";
+import kisses from "./kisses.json";
+import manual from "./manual.json";
+import poetry from "./poetry.json";
 
 export const success = "431825476898652160";
 export const failure = "431825476638474250";
+
+export { hugs, kisses, manual, poetry };
