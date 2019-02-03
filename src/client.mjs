fcc2e6020cc5d48461be92adec2b267584356814
Chen-Pang He <chen.pang.he@jdh8.org>
2019-02-03 23:09:01 +0800

    Tidy up root directory

---
+++ src/client.mjs
@@ -0,0 +1,36 @@
+import * as natsuki from "./natsuki.mjs";
+import Discord from "discord.js";
+import DBL from "dblapi.js";
+
+const client = new Discord.Client();
+const dbl = new DBL(process.env.DBL_TOKEN, client);
+
+const loop = message =>
+{
+	if (message.author.bot)
+		return;
+
+	const match = /^(?:n\.|(<@!?410315411695992833>)\s*)(\S*)\s*([^]*)/.exec(message.content);
+
+	if (match) {
+		const [, mention, command, content] = match;
+		const f = natsuki[command] || (mention && command == "," && content && natsuki.chat);
+		return f ? f(message, content, mention) : mention && message.react("433490397516267532");
+	}
+};
+
+client.on("ready", () => client.user.setActivity("n.help | n.invite"));
+
+client.on("message", async message =>
+{
+	try {
+		await loop(message);
+	}
+	catch (error) {
+		message.channel.send(`${error}`).catch(() => {});
+	}
+});
+
+client.login(process.env.TOKEN);
+
+export default client;
