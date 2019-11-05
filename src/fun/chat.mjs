230296c47a4fd90b698211a78cd1e8421bbb96b3
Chen-Pang He <chen.pang.he@jdh8.org>
2019-11-05 15:20:26 +0800

    Improve error handling in n.chat

--- src/fun/chat.mjs
+++ src/fun/chat.mjs
@@ -3,6 +3,6 @@ import snekfetch from "snekfetch";
 export const chat = async (message, content, mention) =>
 {
 	const { body } = await snekfetch.get(`https://nekos.life/api/v2/chat?text=${content}`);
-	const text = decodeURIComponent(body.response.replace("<334186716770598912>", ""));
+	const text = body.response && decodeURIComponent(body.response.replace("<334186716770598912>", ""));
 	return await (text ? message.channel.send(text) : message.react(mention ? "433490397516267532" : "❓"));
 };
