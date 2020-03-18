b969c38f278b049f087d970be9314edea8172a80
Chen-Pang He <chen.pang.he@jdh8.org>
2020-03-19 03:13:26 +0800

    Optimize n.color with sharp, which has WebP support

--- src/tools/color.mjs
+++ src/tools/color.mjs
@@ -1,25 +1,29 @@
 import Discord from "discord.js";
-import Jimp from "jimp";
+import sharp from "sharp";
 import tinycolor from "tinycolor2";
-import util from "util";
 
-const rgba = ({ r, g, b, a }) => (Math.round(r) << 24 | Math.round(g) << 16 | Math.round(b) << 8 | Math.round(255 * a)) >>> 0;
+const convert = ({ r, g, b, a }) => ({ r, g, b, alpha: a });
 
 export const color = async (message, content) =>
 {
 	const color = new tinycolor(content);
+	const opaque = color.getAlpha() == 1;
 
 	if (!color.isValid())
 		return await message.channel.send(`${content} is not a color.`);
 
-	const description = `**Hex:** ${color.getAlpha() == 1 ? color.toHexString() : color.toHex8String()}
+	const description = `**Hex:** ${opaque ? color.toHexString() : color.toHex8String()}
 **RGB:** ${color.toRgbString()}
 **HSL:** ${color.toHslString()}`;
 
-	const image = new Jimp(128, 128, rgba(color.toRgb()));
-	const buffer = await util.promisify((...x) => image.getBuffer(...x))("image/png");
+	const buffer = sharp({ create: {
+		width: 128,
+		height: 128,
+		channels: 4 - opaque,
+		background: convert(color.toRgb())
+	}}).webp({ lossless: true }).toBuffer();
 
-	return await message.channel.send(description, new Discord.MessageAttachment(buffer, "color.png"));
+	return await message.channel.send(description, new Discord.MessageAttachment(await buffer, "color.webp"));
 };
 
 export const colour = color;
