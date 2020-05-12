43fa4db19150c9d0860281e90435ccb836ab53f6
Chen-Pang He <chen.pang.he@jdh8.org>
2020-05-12 12:29:45 +0800

    Fix index.mjs

--- index.mjs
+++ index.mjs
@@ -2,5 +2,6 @@ import Discord from "discord.js";
 
 const manager = new Discord.ShardingManager("./src/client.mjs", { mode: "worker", token: process.env.TOKEN });
 
+manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
+
 manager.spawn();
-manager.on("launch", shard => console.log(`Launched shard ${shard.id}`));
--- src/index.mjs
+++
@@ -1,7 +0,0 @@
-import Discord from "discord.js";
-
-const manager = new Discord.ShardingManager("./src/client.mjs", { mode: "worker", token: process.env.TOKEN });
-
-manager.on("shardCreate", shard => console.log(`Launched shard ${shard.id}`));
-
-manager.spawn();
