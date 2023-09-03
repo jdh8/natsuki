bdd8d7956b571c2f301cd276f06c74ec57cdbfc6
Chen-Pang He <chen.pang.he@jdh8.org>
2023-09-03 17:19:40 +0800

    Remove unused autocompletion

--- src/core.rs
+++ src/core.rs
@@ -8,7 +8,6 @@ use crate::Context;
 #[poise::command(category = "Core", slash_command)]
 pub async fn help(ctx: Context<'_>,
     #[description = "Specific command to show help about"]
-    #[autocomplete = "poise::builtins::autocomplete_command"]
     command: Option<String>,
 ) -> anyhow::Result<()> {
     poise::builtins::help(
