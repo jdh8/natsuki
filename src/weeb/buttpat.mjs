5da1bc003dfa8232cbb60f3a781d92ad8021eb45
Chen-Pang He <chen.pang.he@jdh8.org>
2023-09-09 01:35:50 +0800

    Remove deprecated/NSFW weeb commands

--- src/weeb/buttpat.mjs
+++
@@ -1,9 +0,0 @@
-export const buttpat = (action, option) =>
-{
-	action.channel.sendTyping();
-
-	return action.reply({ embeds: [{
-		description: `${ action.member ?? action.author } patted ${ (option?.value ?? option) || "Yuzu" } on the butts!`,
-		image: { url: "https://78.media.tumblr.com/165f23ece178a17968de50f084a9ecec/tumblr_p25cyprR041vptudso2_400.gif" },
-	}]});
-};
--- src/weeb/smash.mjs
+++
@@ -1,9 +0,0 @@
-export const smash = (action, option) =>
-{
-	const value = option?.value ?? option;
-
-	return action.reply({ embeds: [{
-		description: `${ action.member ?? action.author } smashed${ value && " " }${ value }!`,
-		image: { url: "https://raw.githubusercontent.com/jdh8/natsuki/master/assets/smash.png" },
-	}]})
-};
